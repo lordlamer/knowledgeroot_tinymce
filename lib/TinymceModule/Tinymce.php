31c197827e482d888cf574b2c205f168567abb9e
lordlamer <lordlamer@lordlamer.de>
2012-12-06 23:20:03 +0100

    * using abstract rte class for tinymce

--- lib/TinymceModule/Tinymce.php
+++ lib/TinymceModule/Tinymce.php
@@ -3,16 +3,20 @@
 /**
  * tinymce class
  */
-class TinymceModule_Tinymce implements Knowledgeroot_Rte_Interface {
+class TinymceModule_Tinymce extends Knowledgeroot_Rte_Abstract {
 	/**
 	 * show editor with content
 	 *
 	 * @param string $content
 	 */
-	public function show($content) {
+	public function show($content = null) {
+	    if($content != null)
+		$this->content = (string) $content;
+
 	    $res = "";
-	    $res .= '<textarea style="width: 650px; height: 300px;" name="content">'.htmlspecialchars($content).'</textarea>';
-	    $res .= '<script type="text/javascript" src="./module/tinymce/tiny_mce/tiny_mce.js"></script>
+	    $res .= '<textarea style="width: 650px; height: 300px;" name="' . $this->name . '">'.htmlspecialchars($this->content).'</textarea>';
+	    $res .= '
+<script type="text/javascript" src="./module/tinymce/tiny_mce/tiny_mce.js"></script>
 <script type="text/javascript">
 tinyMCE.init({
         mode : "textareas"
